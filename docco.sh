8633767ede0b8b69a479b6ef4a745eefebe3fa73
Prajwal K Manjunath <prajwalkman@gmail.com>
2013-06-11 12:23:23 +0530

    update docco script

--- docco.sh
+++ docco.sh
@@ -1,1 +1,1 @@
-node_modules/.bin/docco README.md src/*.litcoffee src/config/parser.litcoffee CONTRIBUTING.md -l linear -t docco_helper/docco.jst -c docco_helper/docco.css
+node_modules/.bin/docco README.md src/*.litcoffee src/prototypes/parser_proto.litcoffee src/prototypes/service_proto.litcoffee CONTRIBUTING.md -l linear -t docco_helper/docco.jst -c docco_helper/docco.css
